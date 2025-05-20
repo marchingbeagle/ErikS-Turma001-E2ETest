9b7c6a8f16db2d8465f6a7bbe9a8eaaed7fef2dd
Erik Schneider <erik.schneider.pacheco@gmail.com>
2025-05-20 19:20:01 -0300

    teste sonar

--- src/scenarios/ZeroStepExample.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-import { ai } from '@zerostep/playwright';
-
-test('ZeroStep Example', async ({ page }) => {
-  await page.goto('https://5elementslearning.dev/demosite/index.php');
-
-  const aiArgs = { page, test };
-  await ai(`click in the courage under fire title`, aiArgs);
-});
